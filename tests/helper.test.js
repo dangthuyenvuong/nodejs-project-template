77220b1c08c276f370717841fc5c77d615c367db
Dang Thuyen Vuong <dangthuyenvuong@gmail.com>
2021-03-01 02:13:39 +0700

    Update test

---
+++ tests/helper.test.js
@@ -0,0 +1,12 @@
+import { objExclude } from '../core/helper'
+
+test('Helper function', async () => {
+    let obj = objExclude({
+        a: 1,
+        b: 2,
+        c: 3,
+        d: 4
+    }, { a: 1, d: 1 })
+
+    expect(obj).toMatchObject({ b: 2, c: 3 })
+})
